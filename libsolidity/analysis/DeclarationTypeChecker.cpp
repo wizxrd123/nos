wranai/solidity

Simplify changes to using for directive.

libsolidity/analysis/DeclarationTypeChecker.cpp
@@ -454,43 +454,36 @@ void DeclarationTypeChecker::endVisit(VariableDeclaration const& _variable)
 
 bool DeclarationTypeChecker::visit(UsingForDirective const& _usingFor)
 {
-	auto checkFunctionDefinition = [&](FunctionDefinition const* _functionDefinition, SourceLocation _location)
+	if (_usingFor.usesBraces())
 	{
-		if (!_functionDefinition->isFree())
-			m_errorReporter.typeError(
-				4167_error,
-				_location,
-				"Only free functions can be bound to a type in a \"using\" statement"
-			);
-	};
-
-	std::visit(util::GenericVisitor{
-		[&](ASTPointer<IdentifierPath> const& _libraryOrFunctionOrModule) {
-			Declaration const* decl = _libraryOrFunctionOrModule->annotation().referencedDeclaration;
-			auto location = _libraryOrFunctionOrModule->location();
-			solAssert(decl, "");
-			if (auto functionDefinition = dynamic_cast<FunctionDefinition const*>(decl))
-				checkFunctionDefinition(functionDefinition, location);
-			else if (auto library = dynamic_cast<ContractDefinition const*>(decl))
+		for (ASTPointer<IdentifierPath> const& function: _usingFor.functions())
+			if (auto functionDefinition = dynamic_cast<FunctionDefinition const*>(function.get()))
 			{
-				if (!library->isLibrary())
-					m_errorReporter.fatalTypeError(4357_error, location, "Library name expected.");
-			}
-			else if (dynamic_cast<ImportDirective const*>(decl))
-			{
+				if (!functionDefinition->isFree() && !(
+					dynamic_cast<ContractDefinition const*>(functionDefinition->scope()) &&
+					dynamic_cast<ContractDefinition const*>(functionDefinition->scope())->isLibrary()
+				))
+					m_errorReporter.typeError(
+						4167_error,
+						function->location(),
+						"Only free functions and library functions can be bound to a type in a \"using\" statement"
+					);
 			}
 			else
-				m_errorReporter.fatalTypeError(8187_error, location, "Expected library name, free-function(s) name(s), module name or *." );
-		},
-		[&](vector<ASTPointer<IdentifierPath>> const& _functionList) {
-			for (auto const& path: _functionList)
-				checkFunctionDefinition(
-					dynamic_cast<FunctionDefinition const*>(path->annotation().referencedDeclaration),
-					path->location()
-				);
-		},
-		[&](UsingForDirective::Asterisk const&) { }
-	}, _usingFor.functions());
+				m_errorReporter.fatalTypeError(8187_error, function->location(), "Expected function name." );
+	}
+	else
+	{
+		ContractDefinition const* library = dynamic_cast<ContractDefinition const*>(
+			_usingFor.functions().front()->annotation().referencedDeclaration
+		);
+		if (!library || !library->isLibrary())
+			m_errorReporter.fatalTypeError(
+				4357_error,
+				_usingFor.functions().front()->location(),
+				"Library name expected. If you want to attach a function, use '{...}'."
+			);
+	}
 
 	// We do not visit _usingFor.functions() because it will lead to an error since
 	// library names cannot be mentioned stand-alone.

libsolidity/ast/ASTJsonConverter.cpp
@@ -311,26 +311,15 @@ bool ASTJsonConverter::visit(InheritanceSpecifier const& _node)
 
 bool ASTJsonConverter::visit(UsingForDirective const& _node)
 {
-	using ReturnType = pair<string, Json::Value>;
-	ReturnType functions = std::visit(GenericVisitor{
-		[&](ASTPointer<IdentifierPath> const& _libraryOrFunctionOrModule) -> ReturnType
-		{
-			return make_pair("libraryName", toJson(*_libraryOrFunctionOrModule));
-		},
-		[&](vector<ASTPointer<IdentifierPath>> const& _functionList) -> ReturnType
-		{
-			return make_pair("functionList", toJson(_functionList));
-		},
-		[&](UsingForDirective::Asterisk const&) -> ReturnType
-		{
-			return make_pair("asterisk", true);
-		}
-	}, _node.functions());
-
-	setJsonNode(_node, "UsingForDirective", {
-		functions,
+	std::vector<pair<string, Json::Value>> attributes = {
 		make_pair("typeName", _node.typeName() ? toJson(*_node.typeName()) : Json::nullValue)
-	});
+	};
+	if (_node.usesBraces())
+		attributes.emplace_back("functionList", toJson(_node.functions()));
+	else
+		attributes.emplace_back("libraryName", toJson(*_node.functions().front()));
+
+	setJsonNode(_node, "UsingForDirective", move(attributes));
 
 	return false;
 }

libsolidity/ast/AST_accept.h
@@ -188,7 +188,7 @@ void UsingForDirective::accept(ASTVisitor& _visitor)
 {
 	if (_visitor.visit(*this))
 	{
-		listAccept(allFunctions(), _visitor);
+		listAccept(functions(), _visitor);
 		if (m_typeName)
 			m_typeName->accept(_visitor);
 	}
@@ -199,7 +199,7 @@ void UsingForDirective::accept(ASTConstVisitor& _visitor) const
 {
 	if (_visitor.visit(*this))
 	{
-		listAccept(allFunctions(), _visitor);
+		listAccept(functions(), _visitor);
 		if (m_typeName)
 			m_typeName->accept(_visitor);
 	}

wizxrd123/nos

libsolidity/analysis/DeclarationTypeChecker.cpp
@@ -452,43 +452,36 @@ void DeclarationTypeChecker::endVisit(VariableDeclaration const& _variable)
 
 bool DeclarationTypeChecker::visit(UsingForDirective const& _usingFor)
 {
-	auto checkFunctionDefinition = [&](FunctionDefinition const* _functionDefinition, SourceLocation _location)
+	if (_usingFor.usesBraces())
 	{
-		if (!_functionDefinition->isFree())
-			m_errorReporter.typeError(
-				4167_error,
-				_location,
-				"Only free functions can be bound to a type in a \"using\" statement"
-			);
-	};
-
-	std::visit(util::GenericVisitor{
-		[&](ASTPointer<IdentifierPath> const& _libraryOrFunctionOrModule) {
-			Declaration const* decl = _libraryOrFunctionOrModule->annotation().referencedDeclaration;
-			auto location = _libraryOrFunctionOrModule->location();
-			solAssert(decl, "");
-			if (auto functionDefinition = dynamic_cast<FunctionDefinition const*>(decl))
-				checkFunctionDefinition(functionDefinition, location);
-			else if (auto library = dynamic_cast<ContractDefinition const*>(decl))
+		for (ASTPointer<IdentifierPath> const& function: _usingFor.functionsOrLibrary())
+			if (auto functionDefinition = dynamic_cast<FunctionDefinition const*>(function.get()))
 			{
-				if (!library->isLibrary())
-					m_errorReporter.fatalTypeError(4357_error, location, "Library name expected.");
-			}
-			else if (dynamic_cast<ImportDirective const*>(decl))
-			{
+				if (!functionDefinition->isFree() && !(
+					dynamic_cast<ContractDefinition const*>(functionDefinition->scope()) &&
+					dynamic_cast<ContractDefinition const*>(functionDefinition->scope())->isLibrary()
+				))
+					m_errorReporter.typeError(
+						4167_error,
+						function->location(),
+						"Only free functions and library functions can be bound to a type in a \"using\" statement"
+					);
 			}
 			else
-				m_errorReporter.fatalTypeError(8187_error, location, "Expected library name, free-function(s) name(s), module name or *." );
-		},
-		[&](vector<ASTPointer<IdentifierPath>> const& _functionList) {
-			for (auto const& path: _functionList)
-				checkFunctionDefinition(
-					dynamic_cast<FunctionDefinition const*>(path->annotation().referencedDeclaration),
-					path->location()
-				);
-		},
-		[&](UsingForDirective::Asterisk const&) { }
-	}, _usingFor.functions());
+				m_errorReporter.fatalTypeError(8187_error, function->location(), "Expected function name." );
+	}
+	else
+	{
+		ContractDefinition const* library = dynamic_cast<ContractDefinition const*>(
+			_usingFor.functionsOrLibrary().front()->annotation().referencedDeclaration
+		);
+		if (!library || !library->isLibrary())
+			m_errorReporter.fatalTypeError(
+				4357_error,
+				_usingFor.functionsOrLibrary().front()->location(),
+				"Library name expected. If you want to attach a function, use '{...}'."
+			);
+	}
 
 	// We do not visit _usingFor.functions() because it will lead to an error since
 	// library names cannot be mentioned stand-alone.

libsolidity/ast/ASTJsonConverter.cpp
@@ -312,26 +312,15 @@ bool ASTJsonConverter::visit(InheritanceSpecifier const& _node)
 
 bool ASTJsonConverter::visit(UsingForDirective const& _node)
 {
-	using ReturnType = pair<string, Json::Value>;
-	ReturnType functions = std::visit(GenericVisitor{
-		[&](ASTPointer<IdentifierPath> const& _libraryOrFunctionOrModule) -> ReturnType
-		{
-			return make_pair("libraryName", toJson(*_libraryOrFunctionOrModule));
-		},
-		[&](vector<ASTPointer<IdentifierPath>> const& _functionList) -> ReturnType
-		{
-			return make_pair("functionList", toJson(_functionList));
-		},
-		[&](UsingForDirective::Asterisk const&) -> ReturnType
-		{
-			return make_pair("asterisk", true);
-		}
-	}, _node.functions());
-
-	setJsonNode(_node, "UsingForDirective", {
-		functions,
+	std::vector<pair<string, Json::Value>> attributes = {
 		make_pair("typeName", _node.typeName() ? toJson(*_node.typeName()) : Json::nullValue)
-	});
+	};
+	if (_node.usesBraces())
+		attributes.emplace_back("functionList", toJson(_node.functionsOrLibrary()));
+	else
+		attributes.emplace_back("libraryName", toJson(*_node.functionsOrLibrary().front()));
+
+	setJsonNode(_node, "UsingForDirective", move(attributes));
 
 	return false;
 }

libsolidity/ast/AST_accept.h
@@ -194,7 +194,7 @@ void UsingForDirective::accept(ASTVisitor& _visitor)
 {
 	if (_visitor.visit(*this))
 	{
-		listAccept(allFunctions(), _visitor);
+		listAccept(functionsOrLibrary(), _visitor);
 		if (m_typeName)
 			m_typeName->accept(_visitor);
 	}
@@ -205,7 +205,7 @@ void UsingForDirective::accept(ASTConstVisitor& _visitor) const
 {
 	if (_visitor.visit(*this))
 	{
-		listAccept(allFunctions(), _visitor);
+		listAccept(functionsOrLibrary(), _visitor);
 		if (m_typeName)
 			m_typeName->accept(_visitor);
 	}